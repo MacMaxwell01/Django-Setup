dd4175a23c9494f177ebb39fc1429bbbc253add7
Oloye <oluwagbemirooloyemaxwell@gmail.com>
2026-08-27 12:59:53 +0100

    Added .env

--- classproject/settings.py
+++ classproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3^u7gm9qk5w$(5qsetuv#97@^oiebb=#@=@j)#(hx=f%(r6#$a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = ["*"]
 

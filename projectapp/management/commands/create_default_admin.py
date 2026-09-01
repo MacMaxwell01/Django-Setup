d4b9aee8f735cf983cfefb074c036aa29943cbd4
Oloye <oluwagbemirooloyemaxwell@gmail.com>
2026-09-01 12:28:30 +0100

    Added Default Admin Creation Script Mdified

--- projectapp/management/commands/create_default_admin.py
+++ projectapp/management/commands/create_default_admin.py
@@ -6,15 +6,15 @@ from django.contrib.auth.models import User
 class Command(BaseCommand):
     help = "Helps to create the default admin user to handle things"
 
+    admin_username = config("DEFAULT_ADMIN_USERNAME")
+    admin_password = config("DEFAULT_ADMIN_PASSWORD")
+
     def handle(self, *args, **kwargs):
-        admin_username = config("DEFAULT_ADMIN_USERNAME", default=None)
-        admin_password = config("DEFAULT_ADMIN_PASSWORD", default=None)
-        
-        if not (admin_username and admin_password):
+        if not (self.admin_username and self.admin_password):
             self.stdout.write(self.style.ERROR("Default admin username and password are not set in the environment variables."))
             return
-        if User.objects.filter(username=admin_username).exists():
-            self.stdout.write(self.style.ERROR(f"Default admin user '{admin_username}' already exists."))
+        if User.objects.filter(username=self.admin_username).exists():
+            self.stdout.write(self.style.ERROR(f"Default admin user '{self.admin_username}' already exists."))
         else:
-            admin_user = User.objects.create_superuser(admin_username, admin_password)
+            admin_user = User.objects.create_superuser(username=str(self.admin_username), password=str(self.admin_password), email=None)
             self.stdout.write(self.style.SUCCESS(f"Default admin user '{admin_user.username}' created successfully."))
